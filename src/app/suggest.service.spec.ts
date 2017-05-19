a150a148926f8d6ea8c3e9ea0232b058fa1874cb
harshit98 <harshitprasad28@gmail.com>
2017-05-19 19:51:45 +0530

    Resolved travis errors

--- src/app/suggest.service.spec.ts
+++ src/app/suggest.service.spec.ts
@@ -1,10 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { SuggestService } from './suggest.service';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { reducer } from './reducers/index';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 describe('SuggestService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpModule,
+        JsonpModule,
+        StoreModule.provideStore(reducer),
+        StoreDevtoolsModule.instrumentOnlyWithExtension(),
+      ],
       providers: [SuggestService]
     });
   });
