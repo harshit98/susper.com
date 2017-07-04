5b70169c05d0f18bd0269b67bb364fc47f2e2f2a
harshit98 <harshit.codepy@gmail.com>
2017-07-04 14:35:47 +0530

    remove imports

--- src/app/searchsettings/searchsettings.component.spec.ts
+++ src/app/searchsettings/searchsettings.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FooterNavbarComponent } from '../footer-navbar/footer-navbar.component';
 import { SearchsettingsComponent } from './searchsettings.component';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { reducer } from '../reducers/index';
+import { StoreModule } from '@ngrx/store';
 
 describe('SearchsettingsComponent', () => {
   let component: SearchsettingsComponent;
@@ -8,7 +12,15 @@ describe('SearchsettingsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchsettingsComponent ]
+      imports: [
+        FormsModule,
+        RouterTestingModule,
+        StoreModule.provideStore(reducer)
+      ],
+      declarations: [
+        SearchsettingsComponent,
+        FooterNavbarComponent
+      ]
     })
     .compileComponents();
   }));
@@ -17,6 +29,41 @@ describe('SearchsettingsComponent', () => {
     fixture = TestBed.createComponent(SearchsettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  it('should create an instance', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should create a FooterNavbar Component', () => {
+    const footerNavbar = new FooterNavbarComponent();
+    expect(footerNavbar).toBeTruthy();
+  });
+
+  it('should have a save button', () => {
+    let compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('button.savbtn'));
+  });
+
+  it('should have a cancel button', () => {
+    let compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('button.cancbtn'));
+  });
+
+  it('should have a navbar', () => {
+    let compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('nav.navbar'));
+  });
+
+  it('should have alt text property as brand', () => {
+    let compiled = fixture.debugElement.nativeElement;
+
+    let image: HTMLImageElement = compiled.querySelector('a.navbar-brand img');
+    expect(image).toBeTruthy();
+    expect(image.alt).toBe('brand');
   });
 
 });
