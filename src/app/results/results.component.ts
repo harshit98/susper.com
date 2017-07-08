229b0826e2d3755e26f5f6cc33cbd23f70ec5be0
harshit98 <harshitprasad28@gmail.com>
2017-07-09 02:51:42 +0530

    Fixes #607: Increase search bar input area

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -188,5 +188,6 @@ export class ResultsComponent implements OnInit {
 
   ngOnInit() {
     document.getElementById('nav-group').style.width = '632px';
+    document.getElementById('nav-input').style.width = '536px';
   }
 }
